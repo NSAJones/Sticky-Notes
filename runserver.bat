e58e60651181d699b3da21e2f024161591848335
NSAJones <nsajones23@gmail.com>
2024-06-24 16:58:58 +0100

    removed static command from server run

--- runserver.bat
+++ runserver.bat
@@ -1,4 +1,3 @@
 python sticky_notes\manage.py makemigrations
 python sticky_notes\manage.py migrate
-python sticky_notes\manage.py collectstatic --noinput --clear
 python sticky_notes\manage.py runserver
