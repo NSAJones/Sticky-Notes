a9aa3ad3521bea84041a63322c7403674c685eaf
NSAJones <nsajones23@gmail.com>
2024-06-24 17:20:55 +0100

    changed runserver.bat to migrate both applications

--- runserver.bat
+++ runserver.bat
@@ -1,3 +1,4 @@
-python sticky_notes\manage.py makemigrations
+python sticky_notes\manage.py makemigrations boards
+python sticky_notes\manage.py makemigrations login
 python sticky_notes\manage.py migrate
 python sticky_notes\manage.py runserver
